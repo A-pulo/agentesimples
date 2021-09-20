fef9247ef4c804d590855921b20bd54c05fb9677
DESKTOP-FV5Q462\Pichau <mineia.mini14@gmail.com>
2021-09-20 20:06:32 -0300

    Agente por objetivo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 def exibir(I):
 
     global posxy
+    global position
 
     # Mostra o ambiente de acordo com a matriz I
     plt.imshow(I, 'gray')
@@ -51,8 +52,120 @@ def agenteReativoSimples(x, y, estado):
     # Ponto de execução da escolha do agente
     return escolher_acao()
 
+def calcularDistancia(x,y,xobjetivo,yobjetivo):
+    distancia = 0
+    while x != xobjetivo:
+        if x > xobjetivo:
+            x -= 1
+            distancia +=1
+        else:
+            x+=1
+            distancia += 1
+    while y != yobjetivo:
+        if y > yobjetivo:
+            y -= 1
+            distancia +=1
+        else:
+            y +=1
+            distancia += 1
+    return distancia
 
-# # Preparação do espaço
+def sujeiraMaisProxima(x,y):
+
+    #Retorna a posição que existe sujeira
+    resultado = np.where(espaco == 2)
+    contador = 0
+
+    #Retorno do método
+    posicaoSujeira = []
+
+    #Número estipulado usando tamanho da matriz
+    menorDistancia = 6 * 6
+
+    #Verifica qual é a sujeira mais proxima da posicao x y
+    while contador < len(resultado[0]):
+        sujeira = [resultado[0][contador], resultado[1][contador]]
+        d = calcularDistancia(y,x,sujeira[0], sujeira[1])
+        if (d < menorDistancia):
+            menorDistancia = d
+            posicaoSujeira.clear()
+            posicaoSujeira.append(sujeira[0])
+            posicaoSujeira.append(sujeira[1])
+        contador += 1
+
+    return posicaoSujeira
+
+def posicaoMaisProximaSujeira(x, y, sujeira, espaco):
+
+    #Todos as posições adjacentes da posição atual (x,y) que não são paredes
+    listaAdjacente = []
+
+    abaixo = [x, y + 1]
+    acima = [x, y -1]
+    esquerda = [x - 1,y]
+    direita =[x+1, y]
+
+    # Número estipulado usando tamanho da matriz
+    menorDistancia = 6 * 6
+
+    #Retorno do método
+    retornoAdjacente = []
+
+    if(espaco[acima[1]][acima[0]] != 1):
+        #Posicao x, y e acao
+        adjacente = [acima[1], acima[0], 0]
+        listaAdjacente.append(adjacente)
+    if(espaco[abaixo[1]][abaixo[0]] != 1):
+        # Posicao x, y e acao
+        adjacente = [abaixo[1], abaixo[0], 1]
+        listaAdjacente.append(adjacente)
+    if (espaco[esquerda[1]][esquerda[0]] != 1):
+        # Posicao x, y e acao
+        adjacente = [esquerda[1], esquerda[0], 2]
+        listaAdjacente.append(adjacente)
+    if (espaco[direita[1]][direita[0]] != 1):
+        # Posicao x, y e acao
+        adjacente = [direita[1], direita[0], 3]
+        listaAdjacente.append(adjacente)
+
+    #Buscar posição adjacente com menor distância da sujeira
+    for item in listaAdjacente:
+        d = calcularDistancia(item[0], item[1], sujeira[0], sujeira[1])
+        if(d < menorDistancia):
+            menorDistancia = d
+            retornoAdjacente.clear()
+            retornoAdjacente.append(item[0])
+            retornoAdjacente.append(item[1])
+            retornoAdjacente.append(item[2])
+
+    return retornoAdjacente
+
+def agenteObjetivo(x, y, estado, objObtido, espaco, sujeira):
+    acoes = ["acima", "abaixo", "esquerda", "direita", "aspirar", "NoOp"]
+
+    if(objObtido == 0):
+        return acoes[5]
+    elif (estado == 2):
+        return acoes[4]
+
+    #Verifica qual é o próximo passo
+    proximaPosicao = posicaoMaisProximaSujeira(x,y, sujeira, espaco)
+
+    return acoes[proximaPosicao[2]]
+
+def checkObj(espaco):
+    for lista in espaco:
+        for elemento in lista:
+            if(elemento == 2):
+                return 1
+    return 0
+
+def geraAmbiente():
+    pass
+
+#Pergunta
+agente  = input("Escolha um agente: Agente Reativo Simples (1) ou Agente Baseado em Objetivo(2)")
+
 # Gera o espaço da sala como uma matriz 6x6
 espaco = [[0 for n in range(7)] for n in range(7)]
 
@@ -72,9 +185,36 @@ posxy = [1, 1]
 x_dir = 1 # 0 = esquerda, 1 = direita
 y_dir = 1 # 0 = acima, 1 = abaixo
 
+pontos = 0
+sujeira = 0
+
 # Execução do ambiente
 while True:
-    acao = agenteReativoSimples(posxy[0], posxy[1],espaco[posxy[1]][posxy[0]])
+
+    #Verifica qual agente utilizar
+    if(agente == "1"):
+        acao = agenteReativoSimples(posxy[0], posxy[1],espaco[posxy[1]][posxy[0]])
+    elif(agente == "2"):
+
+        #Verifica se ainda tem sujeira
+        check = checkObj(espaco)
+
+        #Busca sujeira mais próxima da posição atual
+        if(sujeira == 0):
+            sujeira = sujeiraMaisProxima(posxy[0], posxy[1])
+
+        percepcao = espaco[posxy[1]][posxy[0]]
+        acao = agenteObjetivo(posxy[0], posxy[1], percepcao, check, espaco, sujeira)
+
+
+        if(acao != "NoOp"):
+            pontos += 1
+            print(f"Estado da percepcao: {percepcao} Ação escolhida: {acao}")
+        if(acao == "aspirar"):
+            sujeira = 0
+
+
+    #Verifica as ações
     if(acao == "acima"):
         posxy = [posxy[0], (posxy[1]-1)]
     elif(acao == "abaixo"):
@@ -85,4 +225,7 @@ while True:
         posxy = [(posxy[0]+1), posxy[1]]
     elif(acao == "aspirar"):
         espaco[posxy[1]][posxy[0]] = 0
+    elif(acao == "NoOp"):
+        print(f"Pontos: {pontos}")
+        break
     exibir(espaco)
